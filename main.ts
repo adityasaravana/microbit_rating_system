22a9139cacac649a9067ad44d38c597e9517711b
Aditya Saravana <aditya.saravana@icloud.com>
2023-05-04 10:20:54 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,50 +1,31 @@
+bluetooth.onBluetoothConnected(function () {
+    music.playMelody("C D E F G A B C5 ", 800)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    music.playMelody("C5 B A G F E D C ", 800)
+})
 input.onButtonPressed(Button.A, function () {
     Likes += 1
     music.playTone(523, music.beat(BeatFraction.Quarter))
-    datalogger.log(
-    datalogger.createCV("Dislikes", Dislikes),
-    datalogger.createCV("Likes", Likes)
-    )
+    bluetooth.uartWriteString("Likes: " + Likes + ", Dislikes: " + Dislikes)
 })
 input.onButtonPressed(Button.AB, function () {
     Likes = 0
     Dislikes = 0
     datalogger.deleteLog()
-    datalogger.setColumnTitles(
-    "Likes",
-    "Dislikes",
-    "Light Level",
-    "Sound Level",
-    "Microbit Temperature"
-    )
 })
 input.onButtonPressed(Button.B, function () {
     Dislikes += 1
     music.playTone(131, music.beat(BeatFraction.Quarter))
-    datalogger.log(
-    datalogger.createCV("Dislikes", Dislikes),
-    datalogger.createCV("Likes", Likes)
-    )
+    bluetooth.uartWriteString("Likes: " + Likes + ", Dislikes:" + Dislikes)
 })
 let Dislikes = 0
 let Likes = 0
 let Logging = true
-datalogger.setColumnTitles(
-"Likes",
-"Dislikes",
-"Light Level",
-"Sound Level",
-"Microbit Temperature"
-)
+bluetooth.uartWriteString("Logging Started")
 loops.everyInterval(1000, function () {
     if (Logging) {
-        datalogger.log(
-        datalogger.createCV("Dislikes", Dislikes),
-        datalogger.createCV("Likes", Likes),
-        datalogger.createCV("Light Level", input.lightLevel()),
-        datalogger.createCV("Sound Level", input.soundLevel()),
-        datalogger.createCV("Microbit Temperature", input.temperature())
-        )
+    	
     }
 })
 basic.forever(function () {
