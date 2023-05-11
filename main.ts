d78f0f505d484b180432343d777b5caa7ea2194b
Aditya Saravana <aditya.saravana@icloud.com>
2023-05-11 09:07:47 -0700

    switched back to flash memory logging, but kept BT UART compatibility

--- main.ts
+++ main.ts
@@ -8,20 +8,41 @@ input.onButtonPressed(Button.A, function () {
     Likes += 1
     music.playTone(523, music.beat(BeatFraction.Quarter))
     bluetooth.uartWriteString("Likes: " + Likes + ", Dislikes: " + Dislikes)
+    datalogger.log(
+    datalogger.createCV("Likes", Likes),
+    datalogger.createCV("Dislikes", Dislikes)
+    )
 })
 input.onButtonPressed(Button.AB, function () {
     Likes = 0
     Dislikes = 0
+    datalogger.deleteLog()
+    datalogger.setColumnTitles(
+    "Likes",
+    "Dislikes"
+    )
+    datalogger.log(
+    datalogger.createCV("Likes", Likes),
+    datalogger.createCV("Dislikes", Dislikes)
+    )
 })
 input.onButtonPressed(Button.B, function () {
     Dislikes += 1
     music.playTone(131, music.beat(BeatFraction.Quarter))
     bluetooth.uartWriteString("Likes: " + Likes + ", Dislikes:" + Dislikes)
+    datalogger.log(
+    datalogger.createCV("Likes", Likes),
+    datalogger.createCV("Dislikes", Dislikes)
+    )
 })
 let Dislikes = 0
 let Likes = 0
 let Logging = true
 bluetooth.uartWriteString("Logging Started")
+datalogger.setColumnTitles(
+"Likes",
+"Dislikes"
+)
 loops.everyInterval(1000, function () {
     if (Logging) {
     	
